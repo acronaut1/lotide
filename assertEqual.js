5d8704ce80c2e521076f43b3f9d5a15631876462
Oskar Pruski <opruski@ryerson.ca>
2021-10-26 03:19:19 +0000

    Update: morph concat'D strings into Template Strings..

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // Function Implementation..
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🟩|✨Assertion Passed✨|⭕: " + actual + " === " + expected);
+    console.log(`🟩|✨Assertion Passed✨|⭕: ${actual} === ${expected}`);
   } else {
-    console.log("🟥|💥Assertion Failed💥|❌: " + actual + " !== " + expected);
+    console.log(`🟥|💥Assertion Failed💥|❌: ${actual} !== ${expected}`);
   }
 };
 
