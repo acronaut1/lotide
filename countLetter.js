1f6f6acb3c9ab34d1189a76873296679a00150d6
acronaut1 <opruski@ryerson.ca>
2021-10-29 20:55:06 -0400

    add Count Letters for Strings.. wip

---
+++ countLetter.js
@@ -0,0 +1,30 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🟩 Assertion Passed 🟩: ${actual} === ${expected}`);
+  } else {
+    console.log(`🟥 Assertion Failed 🟥: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (itemsToCount) {
+  const result = {}
+  for (const chr of itemsToCount) {
+    if (result[chr]) {
+      result[chr] += 1
+    } else {
+      // playblast-vis. stepper.. 
+      console.log(result);
+      console.log(result[chr] = 1);
+    }
+  }
+  return result;
+};
+
+//const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+//assertEqual(result1["Jason"], 1);
+
+console.log(countLetters("LHL"));
+console.log(countLetters("Lighthouse"));
+console.log(countLetters("Lighthouse Labs"));
+console.log(countLetters("Light house Labs"));
+console.log(countLetters("Agouhanna"));
