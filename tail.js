5097dfd7695e8697bd9dfde621dcf2a305519152
acronaut1 <opruski@ryerson.ca>
2021-11-01 15:17:30 -0400

    REFACTOR CLEAN  tail, ADD tail-test

--- tail.js
+++ tail.js
@@ -5,19 +5,6 @@ const tail = function(set) {
   return elseSet;
 };
 
-// Function: Assertion Implementation..
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟩|✨Assertion Passed✨|⭕: ${actual} === ${expected}`);
-  } else {
-    console.log(`🟥|💥Assertion Failed💥|❌: ${actual} !== ${expected}`);
-  }
-};
-
-// Test Case: Check the original array..
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
 
 /*// Other Scenarios:..
 const words2 = ["Yo Yo"];
@@ -25,3 +12,5 @@ console.log(tail(words2));
 const words3 = [];
 console.log(tail(words3));
 */
+
+module.exports = tail;
