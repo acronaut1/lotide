5e6656af628fa0cbb34e70f950cc5f4b4b3103df
acronaut1 <opruski@ryerson.ca>
2021-11-01 23:42:23 -0400

    CONVERT to Mocha and Chai Implementation..

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,8 +1,9 @@
-//const eqArrays = require('../eqArrays');
+const eqArrays = require('./eqArrays');
 
 // assertArrayEqual WIP...a.01-base
+// (let i = 0; i < setA.length && setB.length; i++)
 const assertArraysEqual = function (setA, setB) {
-  for (let i = 0; i < setA.length && setB.length; i++) {
+  for (let i = 0; i < setA.length; i++) {
     if (setA[i] !== setB[i]) {
       //[A]-actual  //[B]-expected
       console.log(`🟥|💥Assertion Failed💥|❌: ${setA} !== ${setB}`);
